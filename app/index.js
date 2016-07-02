e2f0d029da0d3c2f954c2d423978af02fcfa05cc
Neil Kistner <neil.kistner@gmail.com>
2016-07-01 22:56:25 -0500

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,66 @@
+'use strict' // eslint-disable-line strict
+
+const assign = require('object-assign')
+const pify = require('pify')
+const yeoman = require('yeoman-generator')
+
+module.exports = class extends yeoman.Base {
+  init() {
+    const prompts = [
+      {
+        name: 'module',
+        message: 'What is your module name?',
+        default: this.appname.replace(/\s/g, '-'),
+      },
+      {
+        name: 'description',
+        message: 'What does your module do?',
+      },
+      {
+        name: 'url',
+        message: 'What is your personal website URL?',
+        store: true,
+      },
+      {
+        name: 'github',
+        message: 'What is your GitHub username?',
+        store: true,
+        default: () => pify(this.user.github.username)(),
+        validate: x => x.length > 0 ? true : 'You have to provide a username!',
+      },
+    ]
+
+    return this.prompt(prompts)
+      .then(props => {
+        const system = {
+          name: this.user.git.name(),
+          email: this.user.git.email(),
+        }
+        const tpl = assign({}, props, system)
+
+        this.fs.copyTpl(
+          this.templatePath('_package.json'),
+          this.destinationPath('package.json'),
+          tpl
+        )
+      })
+  }
+
+  git() {
+    this.spawnCommandSync('git', [ 'init' ])
+  }
+
+  install() {
+    this.npmInstall(
+      [
+        'ava',
+        'eslint',
+        'eslint-config-airbnb',
+        'eslint-config-wyze',
+        'eslint-plugin-import',
+        'eslint-plugin-wyze',
+      ],
+      { 'save-dev': true }
+    )
+  }
+}
---
+++ readme.md
@@ -0,0 +1,40 @@
+# generator-wyze-nm
+
+[![Build Status][travis-image]][travis-url]
+[![npm][npm-image]][npm-url]
+[![dependencies][deps-image]][deps-url]
+[![devDependencies][depsdev-image]][depsdev-url]
+
+> Scaffold out a node module, [wyze](//github.com/wyze) style.
+
+## Install
+
+```
+$ npm install --global yo generator-wyze-nm
+```
+
+## Usage
+
+With [yo](//github.com/yeoman/yo):
+
+```
+$ yo wyze-nm
+```
+
+## License
+
+Copyright © 2016 [Neil Kistner](//github.com/wyze)
+
+Released under the MIT license. See [license](license) for details.
+
+[travis-image]: https://img.shields.io/travis/wyze/generator-wyze-nm.svg?style=flat-square
+[travis-url]: https://travis-ci.org/wyze/generator-wyze-nm
+
+[npm-image]: https://img.shields.io/npm/v/generator-wyze-nm.svg?style=flat-square
+[npm-url]: https://npmjs.com/package/generator-wyze-nm
+
+[deps-image]: https://img.shields.io/david/wyze/generator-wyze-nm.svg?style=flat-square
+[deps-url]: https://david-dm.org/wyze/generator-wyze-nm
+
+[depsdev-image]: https://img.shields.io/david/dev/wyze/generator-wyze-nm.svg?style=flat-square
+[depsdev-url]: https://david-dm.org/wyze/generator-wyze-nm#info=devDependencies
